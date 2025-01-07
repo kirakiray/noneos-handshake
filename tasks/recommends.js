e1a66a6b954f3ee44c8a6cfcce03770b4a3653a6
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-01-07 21:47:39 +0800

    Update recommends.js

--- tasks/recommends.js
+++ tasks/recommends.js
@@ -1,13 +1,18 @@
 import { users } from "../src/user.js";
 
 export default async ({ fromUser, data }) => {
+  // 暂时禁用推荐用户
   return {
     ok: 1,
-    data: Array.from(users.values()).map((user) => {
-      return {
-        data: user.data,
-        sign: user.dataSignature,
-      };
-    }),
+    data: [],
   };
+  // return {
+  //   ok: 1,
+  //   data: Array.from(users.values()).map((user) => {
+  //     return {
+  //       data: user.data,
+  //       sign: user.dataSignature,
+  //     };
+  //   }),
+  // };
 };
